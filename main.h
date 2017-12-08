330bdaf12c602b19bb10f83ed384a5da88da4a0b
abhishandy <abhishek.sandy@gmail.com>
2017-12-08 09:52:35 -0500

    header file for changing parameters

---
+++ main.h
@@ -0,0 +1,7 @@
+const int nMonomers = 200;	// number of monomers in the simulation box, calculate using desired density
+const double density = 0.08;
+#define POLYSTYRENE
+// #define POLYPROPYLENE
+
+const double minDist = 2;
+const int maxTrials = 100;
